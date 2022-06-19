8ef306a1fd0ab81aa14028d50d33e9c2f59d2142
unordinarity <unordinarity@yandex.ru>
2022-06-19 18:11:27 +0300

    wip: build: pipeline setup

---
+++ rollup.config.js
@@ -0,0 +1,55 @@
+const { terser } = require('rollup-plugin-terser');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+
+const plugins = [
+  nodeResolve({ jsnext: true, skip: ['effector'], extensions: ['.js', '.mjs'] }),
+  commonjs({ extensions: ['.js', '.mjs'] }),
+  terser({}),
+];
+
+const input = 'dist/index.cjs';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    input: 'dist/index.js',
+    external: ['effector'],
+    plugins,
+    output: {
+      file: './dist/patronum.js',
+      format: 'es',
+      sourcemap: true,
+      externalLiveBindings: false,
+    },
+  },
+  {
+    input,
+    external: ['effector'],
+    plugins,
+    output: {
+      file: './dist/patronum.cjs',
+      format: 'cjs',
+      freeze: false,
+      exports: 'named',
+      sourcemap: true,
+      externalLiveBindings: false,
+    },
+  },
+  {
+    input,
+    external: ['effector'],
+    plugins,
+    output: {
+      name: 'patronum',
+      file: './dist/patronum.umd.js',
+      format: 'umd',
+      exports: 'named',
+      sourcemap: true,
+      freeze: false,
+      globals: {
+        effector: 'effector',
+      },
+    },
+  },
+];
